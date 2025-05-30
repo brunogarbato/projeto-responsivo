454da2f938272970173653728018fe652e3c63e3
Bruno Garbato Luchtenberg <bruno.garbatoo@gmail.com>
2025-05-30 00:03:00 -0300

    Update javacript.js

--- javacript.js
+++ javacript.js
@@ -1,13 +1,9 @@
 function chamarMenu () {
     let menu = document.getElementById("itens");
-    let botao = document.getElementById("burguer");
     if (menu.style.display == "block") {
         menu.style.display = "none";
-        botao.backgroundColor = "black"
-        ;
     } else {
         menu.style.display = "block";
-        botao.style.backgroundColor = "white";
     }
 
 }
