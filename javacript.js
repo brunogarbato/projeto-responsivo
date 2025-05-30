4b8b321f9fe1338b692b6631705a619cd20a5d41
Bruno Garbato Luchtenberg <bruno.garbatoo@gmail.com>
2025-05-29 23:52:06 -0300

    criando o menu de hamburguer com js

---
+++ javacript.js
@@ -0,0 +1,8 @@
+function chamarMenu () {
+    let menu = document.getElementById("itens");
+    if (menu.style.display == "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
